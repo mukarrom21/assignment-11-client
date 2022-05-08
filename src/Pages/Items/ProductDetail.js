9f91b51aa6a2a24c4009b1588ec6a787b329c7d0
Mukarrom <mukarrom21@yahoo.com>
2022-05-09 00:42:43 +0600

    product details page updated with good design

--- src/Pages/Items/ProductDetail.js
+++ src/Pages/Items/ProductDetail.js
@@ -3,11 +3,103 @@ import { useParams } from "react-router";
 import useProductDetail from "../hooks/useProductDetail";
 
 const ProductDetail = () => {
-  const {productId} = useParams();
+  const { productId } = useParams();
   const [product] = useProductDetail(productId);
+  const {
+    _id,
+    category,
+    name,
+    seller,
+    price,
+    stock,
+    ratings,
+    ratingsCount,
+    img,
+    shipping,
+    quantity,
+    supplier,
+  } = product;
   return (
-  <div>Protected page ProductDetail {product.name}</div>
-  )
+    <div>
+      <section className="text-gray-600 body-font overflow-hidden">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="lg:w-4/5 mx-auto flex flex-wrap">
+            <div className="lg:w-1/2 w-full lg:pr-10 lg:py-6 mb-6 lg:mb-0">
+              <h2 className="text-sm title-font text-gray-500 tracking-widest">
+                {seller}
+              </h2>
+              <h1 className="text-gray-900 text-3xl title-font font-medium mb-4">
+                {name}
+              </h1>
+              <div className="flex mb-4">
+                <a className="flex-grow text-indigo-500 border-b-2 border-indigo-500 py-2 text-lg px-1">
+                  Description
+                </a>
+              </div>
+              <p className="leading-relaxed mb-4">
+                Move and explore. These lightweight hiking shoes deliver
+                next-generation comfort and freedom of movement for hikes both
+                long and short. The adidas Primeknit upper hugs your foot for
+                flexible, all-terrain stability, while the Boost midsole
+                delivers endless energy to keep you moving up or down the trail.
+                A GORE-TEX membrane sheds water while letting your feet breathe
+                for all-conditions wear.
+              </p>
+              <div className="flex border-t border-gray-200 py-2">
+                <span className="text-gray-500">Product Id</span>
+                <span className="ml-auto text-gray-900">{_id}</span>
+              </div>
+              <div className="flex border-t border-gray-200 py-2">
+                <span className="text-gray-500">Category</span>
+                <span className="ml-auto text-gray-900">{category}</span>
+              </div>
+              <div className="flex border-t border-gray-200 py-2">
+                <span className="text-gray-500">Supplier</span>
+                <span className="ml-auto text-gray-900">{supplier}</span>
+              </div>
+              <div className="flex border-t border-gray-200 py-2">
+                <span className="text-gray-500">Quantity</span>
+                <span className="ml-auto text-gray-900">{stock}</span>
+              </div>
+              <div className="flex border-t border-gray-200 py-2">
+                <span className="text-gray-500">Shipping</span>
+                <span className="ml-auto text-gray-900">{shipping}</span>
+              </div>
+              <div className="flex border-t border-b mb-6 border-gray-200 py-2">
+                <span className="text-gray-500">Rattings</span>
+                <span className="ml-auto text-gray-900">{ratingsCount}</span>
+              </div>
+              <div className="flex">
+                <span className="title-font font-medium text-2xl text-gray-900">
+                  ${price}
+                </span>
+                <button className="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">
+                  delivered
+                </button>
+                <button className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
+                  <svg
+                    fill="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    className="w-5 h-5"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
+                  </svg>
+                </button>
+              </div>
+            </div>
+            <img
+              alt="ecommerce"
+              className="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded"
+              src={img}
+            />
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default ProductDetail;
